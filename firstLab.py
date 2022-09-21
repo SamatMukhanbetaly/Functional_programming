e1b0fb4e299c3bb4d4d0cc92b3a0fc7bd1b12109
Mukhanbetaly Samat <samat.muhanbetaly@gmail.com>
2022-09-21 10:48:05 +0500

    add new file is second lab

--- firstLab.py
+++ firstLab.py
@@ -1,1 +1,8 @@
 print("Hello world");
+
+def level(number):
+    state == "Вы в десятом этаже"  if number >= 1000 else state == "Вы ниже десятого этажа"
+    return state
+
+number = int(input("Введите номер кабинета :"))
+level(number)
